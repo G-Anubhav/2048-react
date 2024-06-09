58625b84aeae33ad8f01a0a68d81c9d6defab147
G-Anubhav <anubhavgoyal868@gmail.com>
2024-06-09 18:43:09 +0530

    initial commit

---
+++ src/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './footer.css';
+
+const Footer = () => {
+
+  return (
+  <div className="footer">
+  <div>
+    <p>&copy; {new Date().getFullYear()} Copyright: Anubhav Goyal{' '}</p>
+  </div>
+  </div>
+
+)};
+
+export default Footer;
+
